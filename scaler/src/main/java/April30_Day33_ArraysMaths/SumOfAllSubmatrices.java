fd9a282fc356b349d377422be10f8d97e95409bb
mithunnn <mithun.nn@paytm.com>
2021-08-23 17:54:33 +0530

    SumOfAllSubmatrices

---
+++ scaler/src/main/java/April30_Day33_ArraysMaths/SumOfAllSubmatrices.java
@@ -0,0 +1,20 @@
+package April30_Day33_ArraysMaths;
+
+import java.util.ArrayList;
+
+public class SumOfAllSubmatrices {
+
+  public static void main(String[] args) {
+
+  }
+  public int solve(ArrayList<ArrayList<Integer>> A) {
+    int N=A.size();
+    int sum=0;
+    for (int i=0;i<A.size();i++){
+      for (int j=0;j<A.get(i).size();j++){
+        sum+=A.get(i).get(j)* (i+1)*(j+1)*(N-i)*(N-j);
+      }
+    }
+    return sum;
+  }
+}
